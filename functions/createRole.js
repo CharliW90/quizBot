579c0ac29ba911c9688d99240a79cda29aadcee6
CharliW90 <acey.williams@googlemail.com>
2024-05-01 16:32:49 +0100

    fix: use else for clarity

--- functions/createRole.js
+++ functions/createRole.js
@@ -4,12 +4,13 @@ module.exports = async (interaction, role) => {
   const roleExists = findRole(interaction, role.name);
   if(roleExists){
     return {error: "Role already exists", role: roleExists};
-  }
-  if(role.name) {
-    const newRole = await interaction.guild.roles.create(role);
-    return {error: null, role: newRole}
   } else {
-    return {error: "Role needs at least a name"}
+    if(role.name) {
+      const newRole = await interaction.guild.roles.create(role);
+      return {error: null, role: newRole}
+    } else {
+      return {error: "Role needs at least a name"}
+    }
   }
 }
 
