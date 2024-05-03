ed5c43172bd5727a50e5c99d5cbb9ae847ba614a
CharliW90 <acey.williams@googlemail.com>
2024-05-03 16:11:58 +0100

    add simple authorisation to apps script

--- cloud_app/clasp/Code.js
+++ cloud_app/clasp/Code.js
@@ -1,10 +1,22 @@
-const key = process.env.forms;
+const key = JSON.parse(PropertiesService.getScriptProperties().getProperty("forms"))
+const pass = PropertiesService.getScriptProperties().getProperty("pass")
 
 function doGet(e) {
-  const roundNumber = e.parameter.formId
-  const formId = key[roundNumber]
-  const form = FormApp.openById(formId);
-  const responses = form.getResponses();
-  const result = JSON.stringify(responses)
-  return ContentService.createTextOutput(result).setMimeType(ContentService.MimeType.JSON);
+  if(e.parameter.passKey === pass){
+    const roundNumber = e.parameter.formId
+    const formId = key[roundNumber]
+    const form = FormApp.openById(formId);
+    const responses = form.getResponses();
+    const result = JSON.stringify(responses)
+    return ContentService.createTextOutput(result).setMimeType(ContentService.MimeType.JSON);
+  } else {
+    err = [
+      "error",
+      {
+        "code": 403,
+        "reason": "password incorrect"
+      }
+    ]
+    return ContentService.createTextOutput(JSON.stringify(err)).setMimeType(ContentService.MimeType.JSON);
+  }
 }
