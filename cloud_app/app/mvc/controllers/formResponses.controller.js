5f9a15b18e11e55d170ef01d65d8810f7a6ca5d1
CharliW90 <acey.williams@googlemail.com>
2024-05-07 16:44:36 +0100

    add: query and parameter checks

--- cloud_app/app/mvc/controllers/formResponses.controller.js
+++ cloud_app/app/mvc/controllers/formResponses.controller.js
@@ -2,28 +2,23 @@ const { newPassword } = require("../../utility/hotPass.js");
 const { fetchFormResponses } = require("../models/formResponses.model.js");
 
 exports.fetchResponse = (req, res, next) => {
-  if(req.query.passkey === process.env.apiPasskey){
-    const {roundNumber} = req.params;
-    console.log(typeof(roundNumber));
-    if(typeof(roundNumber) === 'number'){
-      console.log("roundnumber is number")
-    } else {
-      if(roundNumber === "all"){
-        console.log("all rounds wanted")
-      }
-    }
-
-    const password = newPassword();
-
-    fetchFormResponses(`https://script.google.com/macros/s/${process.env.scriptId}/exec?formId=${roundNumber}&passkey=${password}`)
-    .then((data) => {
-      // no need to JSON.parse the data thanks to app.use(express.json()) in our app.js file
-      res.status(200).send(data);
-    })
-    .catch((err) => {
-        next(err);
-    })
-  } else {
+  if(req.query.passkey !== process.env.apiPasskey){
     next({status: 401, msg: "You have not provided valid access rights to this endpoint."})
-  }
+  } else {
+    const {roundNumber} = req.params;
+    if(roundNumber !== "all" && Number(roundNumber) === NaN){
+      next({status: 404, msg: "Invalid roundnumber parameter"})
+    } else {
+      const password = newPassword();
+  
+      fetchFormResponses(`https://script.google.com/macros/s/${process.env.scriptId}/exec?formId=${roundNumber}&passkey=${password}`)
+      .then((data) => {
+        // no need to JSON.parse the data thanks to app.use(express.json()) in our app.js file
+        res.status(200).send(data);
+      })
+      .catch((err) => {
+        next(err);
+      })
+    };
+  };
 };
