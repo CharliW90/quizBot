2f241ccdf6d5a835c103a81f30ec648a71d421dc
CharliW90 <acey.williams@googlemail.com>
2024-05-15 16:58:40 +0100

    fix: typo

--- __tests__/bot.maps.test.js
+++ __tests__/bot.maps.test.js
@@ -1,12 +1,12 @@
 const { teamFromChannel, registerTeam, channelFromTeam } = require("../functions/maps/teamChannels")
 
 describe('teamChannels.js', () => {
-  test('team from channel returns a team name, which is a discord role', () => {
+  test('team from channel returns a team name', () => {
     registerTeam("abc", "123");
     const team = teamFromChannel('123');
     expect(team).toEqual("abc");
   })
-  test('team from channel returns a team name, which is a discord role', () => {
+  test('channel from team returns a channel', () => {
     registerTeam("def", "456");
     const channel = channelFromTeam('def');
     expect(channel).toEqual("456");
