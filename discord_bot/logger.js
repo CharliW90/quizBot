07fc07b3e5767b770ef7358e1169b9ea3afd922b
CharliW90 <acey.williams@googlemail.com>
2024-07-05 17:25:51 +0100

    switched to using logger file to declare transport, instead of npx in deploy script

--- discord_bot/logger.js
+++ discord_bot/logger.js
@@ -1,3 +1,14 @@
 const pino = require('pino');
 
-module.exports = pino({});
+const logger = pino({
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      levelFirst: true,
+      colorize: true,
+      ignore: 'pid,hostname',
+    }
+  }
+})
+
+module.exports = logger;
