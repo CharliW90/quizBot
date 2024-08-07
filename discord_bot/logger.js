1b9e6785d69334fe55c6b92c61b24b3a954a7f66
CharliW90 <acey.williams@googlemail.com>
2024-08-07 16:51:18 +0100

    update log handling

--- discord_bot/logger.js
+++ discord_bot/logger.js
@@ -1,60 +1,124 @@
 const pino = require('pino');
+const { isColorSupported } = require('pino-pretty')
+const { PermissionFlagsBits } = require('discord.js');
 
 const transports = pino.transport({
   targets: [{
     level: 'error',
     target: 'pino-pretty',
     options: {
-      levelFirst: true,
-      colorize: true,
-      ignore: 'pid,hostname',
-    }
+      ignore: 'pid,hostname,path,code,message',
+      singleLine: false,
+    },
   },
   {
     level: 'debug',
     target: 'pino-pretty',
     options: {
-      levelFirst: true,
-      colorize: true,
-      ignore: 'pid,hostname',
-    }
+      ignore: 'pid,hostname,stack',
+    },
   },
   {
     target: 'pino-pretty',
     options: {
-      levelFirst: true,
-      colorize: true,
-      ignore: 'pid,hostname,command,path,fn',
-    }
+      ignore: 'pid,hostname,slashCommand,path,fn,stack,source,code,message,details',
+    },
   }],
   dedupe: true
 })
 
-const logger = pino({}, transports)
+const logger = pino({
+  mixin(obj, num) {
+    if(obj.code && obj.message && num === 50){
+      errCode = obj.code;
+      errMsg = obj.message;
+      return {msg: `${errCode}: ${errMsg}`}
+    } else {
+      if(obj.message){
+        return { msg: obj.message}
+      } else {
+        return { msg: obj.msg}
+      }
+    }
+  },
+},transports)
+
+const buildCommandDetails = (localDetails) => {
+  const {category, data, execute, autocomplete} = localDetails
+  const {name, default_member_permissions, options} = data
+  const executeDefinition = String(execute).split("execute(")[1].split(")")[0]
+  const executeArgs = executeDefinition.split(",").map(arg => arg.split("=")[0])
+  const details = {
+    commandName: `${category}/${name}`,
+    adminOnly: default_member_permissions <= PermissionFlagsBits.ManageChannels,
+    userInput: options,
+    execute: {
+      args: executeArgs
+    },
+    autocomplete: typeof(autocomplete) !== 'undefined'
+  }
+  if(autocomplete){
+    const autocompleteDefinition = String(autocomplete).split("autocomplete(")[1].split(")")[0]
+    const autocompleteArgs = autocompleteDefinition.split(",").map(arg => arg.split("=")[0])
+    details.autocomplete = {
+      args: autocompleteArgs
+    }
+  }
+  return details
+}
+
+const buildFunctionDetails = (properties) => {
+  const details = []
+  for(key in Object.keys(properties)){
+    property = properties[key]
+    const {id, path, filename} = property
+    if(id && path && filename){
+      const filePath = path.split("discord_bot/")[1] + "/"
+      const fileName = filename.split(filePath)[1]
+      details.push({fileName, filePath})
+    }
+    if(typeof(property) === 'string'){
+      details.push(property)
+    }
+  }
+  return details
+}
 
 /**
+ * Creates a new pino logger - Example usage: ```const logger = localisedLogging(new Error(), arguments, this)```
+ *
+ * logger.info() produces minimal log message with no additional details
+ * 
+ * logger.debug() (suppressed by default) produces log message with additional details about functions and function calls
+ * 
+ * logger.error() produces log message with additional details, including error stack and functions and variables
+ *
  * @generator
- * @param {Error} error a new Error() object, from which various details can be inferred
- * @param {Array} args  a discord interaction object
+ * @param {Error} error - a 'new Error()' object
+ * @param {IArguments} localArgs - the local 'arguments' object
+ * @param {object} globalDetails - the global 'this' object
  * @returns {pino.Logger}
  */
-const localisedLogging = (error, args) => {
+const localisedLogging = (error, localArgs, globalDetails) => {
   const stackArray = error.stack.split('\n');
+  const stack = stackArray.slice(1, -1).map(line => line.trim())
   const location = stackArray[1].trim()
   const details = location.split(" ")
-  const arguments = Object.values(args).map((arg) => {return arg.constructor.name ? arg.constructor.name : typeof(arg)})
+  const arguments = Object.values(localArgs).map((arg) => {return arg.constructor.name ? arg.constructor.name : typeof(arg)})
 
   const filePath = details[2] ? details[2].split(":")[0] : "unknown"
   const functionName = details[1] ? `${details[1]}(${arguments})` : "unknown"
 
   const path = filePath.split("discord_bot/")[1]
   const fn = functionName.replace("Object", path.split('/').pop().split('.js')[0])
-  const command = path.split('/')[0] === 'commands'
+  const slashCommand = path.split('/')[0] === 'commands'
 
   return logger.child({
-    command,
+    slashCommand,
     path,
     fn,
+    source: slashCommand ? buildCommandDetails(globalDetails) : buildFunctionDetails(localArgs),
+    stack,
     level: logger.level
   })
 }
