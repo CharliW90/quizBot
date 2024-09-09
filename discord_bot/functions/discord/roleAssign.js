0d33f3de56279a74f0ee799c3b04077176822c0b
CharliW90 <acey.williams@googlemail.com>
2024-09-09 12:00:24 +0100

    fix: switch from '.forEach' to a 'for' loop in order to await changes on the Discord API

--- discord_bot/functions/discord/roleAssign.js
+++ discord_bot/functions/discord/roleAssign.js
@@ -1,8 +1,8 @@
 module.exports = async (role, members) => {
   try{
-    members.forEach((member) => {
-      member.roles.add(role)
-    })
+    for(let i = 0; i < members.length; i++){
+      await members[i].roles.add(role)
+    }
     return {error: null, response: role};
   } catch(error){
     return {error, response: null};
--- discord_bot/functions/discord/roleRemove.js
+++ discord_bot/functions/discord/roleRemove.js
@@ -1,8 +1,8 @@
-module.exports = (role, members) => {
+module.exports = async (role, members) => {
   try{
-    members.forEach((member) => {
-      member.roles.remove(role)
-    })
+    for(let i = 0; i < members.length; i++){
+      await members[i].roles.remove(role)
+    }
     return {error: null, response: {role, "count": members.length}};
   } catch(error){
     return {error, response: null};
