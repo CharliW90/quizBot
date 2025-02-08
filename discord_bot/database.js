08ba433a8ea420a74d3d0e2b126497d46fc0f34b
CharliW90 <acey.williams@googlemail.com>
2025-02-08 11:06:27 +0000

    fix(tests): fix tests for v3 in preparation for moving to v4

--- discord_bot/database.js
+++ discord_bot/database.js
@@ -7,14 +7,14 @@ const logger = localisedLogging(new Error(), arguments, this)
 firebase.initializeApp({
   credential: firebase.credential.cert(googleApiAccess.serviceAccount)
 });
-logger.debug({msg: `initialised firebase:`, firebase})
+logger.debug({ msg: `initialised firebase:`, firebase })
 
 exports.firestore = firebase.firestore();
 
 exports.quizDate = () => {
   const now = new Date(Date.now());
   // allows for up to 4am to be considered as the previous day's quiz
-  now.setHours(now.getHours()-4);
-  logger.debug({msg: `quizDate():`, returnValue: {code: now.toISOString().slice(0,10), name: now.toDateString()}})
-  return {code: now.toISOString().slice(0,10), name: now.toDateString()};
+  now.setHours(now.getHours() - 4);
+  logger.debug({ msg: `quizDate():`, returnValue: { code: now.toISOString().slice(0, 10), name: now.toDateString() } })
+  return { code: now.toISOString().slice(0, 10), name: now.toDateString() };
 };
