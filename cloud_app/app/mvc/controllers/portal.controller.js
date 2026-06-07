79ef2b7a71772cc9a2305349db7e08867a388f1d
Charli Williams <acey.williams@googlemail.com>
2026-06-07 09:27:12 +0100

    chore: reset for V4 rewrite — see v3-stable for archived production code

--- cloud_app/app/mvc/controllers/portal.controller.js
+++
@@ -1,5 +0,0 @@
-const { ping } = require("../models/health.model.js");
-
-exports.home = (req, res, next) => {
-  res.status(501).send("This endpoint is under development.")
-};
--- cloud_app/app/mvc/models/health.model.js
+++
@@ -1,8 +0,0 @@
-const instance = require("../../utility/connection.js")
-
-exports.ping = (url) => {
-  return instance.get(url)
-  .then(response => {
-    return response;
-  })
-}
